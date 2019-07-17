90c5ccb534a283baaee1fd5f9ccd989c9a0869eb
Richard <wei.richard.s@gmail.com>
2019-07-17 12:43:55 -0700

    update schema post-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_072221) do
+ActiveRecord::Schema.define(version: 2019_07_17_013659) do
 
-  create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "phrase"
     t.string "pinyin"
     t.integer "view_count"
