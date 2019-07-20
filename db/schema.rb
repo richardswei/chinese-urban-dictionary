995c7fa2ab3a1fb19c89c125097e0bef46cef852
Richard <wei.richard.s@gmail.com>
2019-07-20 00:43:15 -0700

    add definition migration with schema result

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_013659) do
+ActiveRecord::Schema.define(version: 2019_07_20_072331) do
+
+  create_table "definitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.text "definition"
+    t.text "usage"
+    t.text "usage_translation"
+    t.bigint "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_definitions_on_entry_id"
+  end
 
   create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "phrase"
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_07_17_013659) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "definitions", "entries"
 end
