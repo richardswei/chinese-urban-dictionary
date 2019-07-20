2626a3b2f8926e9fe2ae068735f9f813143f7379
Richard <wei.richard.s@gmail.com>
2019-07-20 15:01:16 -0700

    add methods to controller for definitions

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -1,2 +1,14 @@
 class DefinitionsController < ApplicationController
+	def create 
+		@definition = Definition.new(
+			definition_params
+		)
+		@definition.entry_id = params[:entry_id]
+		@definition.save
+		redirect_to entry_path(@definition.entry)
+	end
+
+	def definition_params
+		params.require(:definition).permit(:definition, :usage, :usage_translation)
+	end
 end
