3e133028a04e8f09d2a8dc194e4cb9fac581e65c
Richard <wei.richard.s@gmail.com>
2019-09-17 16:13:53 -0700

    add migration for adding username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_222145) do
+ActiveRecord::Schema.define(version: 2019_09_17_231208) do
 
   create_table "definitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.text "definition"
@@ -55,8 +55,10 @@ ActiveRecord::Schema.define(version: 2019_09_16_222145) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "definitions", "entries"
