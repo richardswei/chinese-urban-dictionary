ed2b280f961a2792842b45714504b434d7bd1b58
Richard <wei.richard.s@gmail.com>
2019-09-17 17:46:25 -0700

    add migration: debug NoMethodError upon account creation by adding trackables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,38 @@
 class User < ApplicationRecord
+	attr_accessor :login
+
+	def login
+	  @login || self.username || self.email
+	end
+
   has_many :definitions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :authentication_keys => {email: true, login: false}
+         # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+	validate :validate_username
+
+	def validate_username
+	 if User.where(email: username).exists?
+	   errors.add(:username, :invalid)
+	 end
+	end
+
+	def self.find_first_by_auth_conditions(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    if conditions[:username].nil?
+	      where(conditions).first
+	    else
+	      where(username: conditions[:username]).first
+	    end
+	  end
+	end
+
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_231208) do
+ActiveRecord::Schema.define(version: 2019_09_18_004242) do
 
   create_table "definitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.text "definition"
@@ -56,8 +56,11 @@ ActiveRecord::Schema.define(version: 2019_09_17_231208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
