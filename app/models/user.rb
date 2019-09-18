ffb19af779f8303efb9033048ec0bd580dcf0fb4
Richard <wei.richard.s@gmail.com>
2019-09-18 14:32:13 -0700

    fix login issues. unanswered questions about the extra methods in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
-         :validatable, :authentication_keys => {email: true, login: false}
+         :validatable, authentication_keys: [:login]
          # Only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-	validate :validate_username
-
-	def validate_username
-	 if User.where(email: username).exists?
-	   errors.add(:username, :invalid)
-	 end
-	end
 
 	def self.find_first_by_auth_conditions(warden_conditions)
 	  conditions = warden_conditions.dup
@@ -32,6 +25,48 @@ class User < ApplicationRecord
 	      where(username: conditions[:username]).first
 	    end
 	  end
+	end
+
+	protected
+
+	# Attempt to find a user by it's email. If a record is found, send new
+	# password instructions to it. If not user is found, returns a new user
+	# with an email not found error.
+	def self.send_reset_password_instructions attributes = {}
+	  recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+	  recoverable.send_reset_password_instructions if recoverable.persisted?
+	  recoverable
+	end
+
+	def self.find_recoverable_or_initialize_with_errors required_attributes, attributes, error = :invalid
+	  (case_insensitive_keys || []).each {|k| attributes[k].try(:downcase!)}
+
+	  attributes = attributes.slice(*required_attributes)
+	  attributes.delete_if {|_key, value| value.blank?}
+
+	  if attributes.keys.size == required_attributes.size
+	    if attributes.key?(:login)
+	      login = attributes.delete(:login)
+	      record = find_record(login)
+	    else
+	      record = where(attributes).first
+	    end
+	  end
+
+	  unless record
+	    record = new
+
+	    required_attributes.each do |key|
+	      value = attributes[key]
+	      record.send("#{key}=", value)
+	      record.errors.add(key, value.present? ? error : :blank)
+	    end
+	  end
+	  record
+	end
+
+	def self.find_record login
+	  where(["username = :value OR email = :value", {value: login}]).first
 	end
 
 	
