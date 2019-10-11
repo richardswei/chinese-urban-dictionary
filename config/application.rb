766c4acee82f2aa8fcfb6cff2c3a408df4b82572
Richard <wei.richard.s@gmail.com>
2019-10-11 16:45:06 -0700

    use rack-cors

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module ChineseUrbanDictionary
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.insert_before 0, Rack::Cors do
+          allow do
+            origins '*'
+            resource '*', :headers => :any, :methods => [:get, :post, :options]
+          end
+        end
   end
 end
