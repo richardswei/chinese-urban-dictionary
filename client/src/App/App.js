9579c3b000991449bf25a981c8eec935dad814be
Richard <wei.richard.s@gmail.com>
2019-10-16 00:35:55 -0700

    start authetication via cookies

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {Container} from 'react-bootstrap';
+import { instanceOf } from 'prop-types'
+import { withCookies, Cookies } from 'react-cookie'
+
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+
 import About from '../About/About.js';
 import Home from '../Home/Home.js';
 import Entry from '../Entry/Entry.js';
@@ -10,17 +14,94 @@ import SearchResults from '../SearchResults/SearchResults.js'
 import NewEntry from '../Entry/NewEntry.js'
 import Login from '../Sessions/Login.js'
 import Signup from '../Sessions/Signup.js'
+import AuthSignIn from './AuthSignIn.js'
+import AuthSignOut from './AuthSignOut.js'
+
 import '../App/App.css';
+
+const Api = require('../lib/Api.js');
 
 const Background = {
   backgroundSize: 'cover', 
   backgroundPosition: 'center center',
   backgroundRepeat: 'no-repeat',
   height: '100vh',
-
 };
 
 class App extends Component {
+  
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  defaultState() {
+    return {
+      cookieName: 'rails-react-token-auth-jwt',
+      email: undefined,
+      jwt: undefined,
+      user_id: undefined,
+    }
+  }
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = this.defaultState()
+
+    this.propagateSignIn = this.propagateSignIn.bind(this)
+    this.propagateSignOut = this.propagateSignOut.bind(this)
+  }
+
+  propagateSignIn(jwt, history = undefined) {
+    const { cookies } = this.props
+    cookies.set(this.state.cookieName, jwt, { path: '/' })
+    this.getUser(history)
+  }
+
+  propagateSignOut(history = undefined) {
+    const { cookies } = this.props
+    cookies.remove(this.state.cookieName)
+    this.setState({
+      email: undefined,
+      user_id: undefined,
+      jwt: undefined
+    })
+    if (history) history.push('/')
+  }
+
+
+  
+  getUser(history = undefined) {
+    const { cookies } = this.props
+    let jwt = cookies.get(this.state.cookieName)
+    if (!jwt) return null
+
+    Api.getCurrentUser(jwt).then(response => {
+      if (response !== undefined) {
+        this.setState({
+          email: response.email,
+          user_id: response.id,
+          jwt: jwt
+        })
+        if (history) history.push('/')
+      }
+      else {
+        // user has cookie but cannot load current user
+        cookies.remove(this.state.cookieName)
+        this.setState({
+          email: undefined,
+          user_id: undefined,
+          jwt: undefined
+        })
+      }
+    })
+  }
+
   render() {
     return (
       
@@ -37,6 +118,23 @@ class App extends Component {
               <Route path="/login" exact component={Login}/>
               <Route path="/signup" exact component={Signup}/>
               <Route component={NotFound} />
+              {!this.state.jwt &&
+                <Route
+                  exact path="/sign-in"
+                  render={(routeProps) => (
+                    <AuthSignIn {...routeProps} propagateSignIn={this.propagateSignIn} />
+                  )}
+                />
+              }
+
+              {this.state.jwt &&
+                <Route
+                  exact path="/sign-out"
+                  render={(routeProps) => (
+                    <AuthSignOut {...routeProps} propagateSignOut={this.propagateSignOut} />
+                  )}
+                />
+              }
 
             </Switch>
           </Container>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App.js';
+import App from './App/Main.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
