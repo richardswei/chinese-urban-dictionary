bab2d322995e34da419295efc9fc3caea58a1bd2
Richard <wei.richard.s@gmail.com>
2019-10-22 21:31:42 -0700

    do not attempt to modify the columns for ar_internal_metadata

--- db/migrate/20190717013659_make_encoding_utf8mb4.rb
+++ db/migrate/20190717013659_make_encoding_utf8mb4.rb
@@ -5,21 +5,23 @@ class MakeEncodingUtf8mb4 < ActiveRecord::Migration[5.2]
 
 	    execute "ALTER DATABASE `#{db.current_database}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 	    db.tables.each do |table|
-	      execute "ALTER TABLE `#{table}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    		unless table=='ar_internal_metadata'
+		      execute "ALTER TABLE `#{table}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
-	      db.columns(table).each do |column|
-	        case column.sql_type
-	          when /([a-z]*)text/i
-	            default = (column.default.nil?) ? '' : "DEFAULT '#{column.default}'"
-	            null = (column.null) ? '' : 'NOT NULL'
-	            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{column.sql_type.upcase} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci #{default} #{null};"
-	          when /varchar\(([0-9]+)\)/i
-	            sql_type = column.sql_type.upcase
-	            default = (column.default.nil?) ? '' : "DEFAULT '#{column.default}'"
-	            null = (column.null) ? '' : 'NOT NULL'
-	            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{sql_type} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci #{default} #{null};"
-	        end
-	      end
+		      db.columns(table).each do |column|
+		        case column.sql_type
+		          when /([a-z]*)text/i
+		            default = (column.default.nil?) ? '' : "DEFAULT '#{column.default}'"
+		            null = (column.null) ? '' : 'NOT NULL'
+		            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{column.sql_type.upcase} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci #{default} #{null};"
+		          when /varchar\(([0-9]+)\)/i
+		            sql_type = column.sql_type.upcase
+		            default = (column.default.nil?) ? '' : "DEFAULT '#{column.default}'"
+		            null = (column.null) ? '' : 'NOT NULL'
+		            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{sql_type} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci #{default} #{null};"
+		        end
+		      end
+    		end
 	    end
 
 	end
